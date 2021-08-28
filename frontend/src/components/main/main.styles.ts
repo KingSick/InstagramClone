a27bc50b9b99286d6a74a5483da2dd26b8c79782
woochan <vdncks25@naver.com>
2021-08-28 17:03:14 +0900

    [Front] change root folder name (front → frontend)

---
+++ frontend/src/components/main/main.styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  overflow: hidden;
+`;
+
+export const MainWrapper = styled.main`
+  display: flex;
+`;
+
+export const MainArticle = styled.article`
+  display: flex;
+`;
