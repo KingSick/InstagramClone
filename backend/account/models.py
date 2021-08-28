a0af26f3142afa369d8d5cc20304bc66776755af
mint <pb1123love@gmail.com>
2021-08-28 16:37:13 +0900

    Member, Follow 모델 정의, Feed, HashTag 모델 정의, Views Template 추가

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Member(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    image_url = models.CharField(max_length=255)
+    comment = models.TextField(null=True, blank=True)
+
+
+class Follow(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    follow = models.ForeignKey(Member, on_delete=models.CASCADE)
---
+++ backend/feed/follow_feed/views.py
@@ -0,0 +1,67 @@
+# from rest_framework import permissions
+from rest_framework.views import APIView
+
+from feed.models import Feed
+from feed.serializers import FeedSerializer
+
+
+class TemplateView(APIView):
+
+    # view endpoint 접근권한 설정
+    # permissions.IsAuthenticated: 로그인한 사람
+    # permissions.IsAdmin: staff 권한 이상
+    # permission_classes = [permissions.IsAuthenticated]
+
+    @staticmethod
+    def get(request):
+        param = request.query_params.get('key_name')
+        print(f'get param: {param}')
+
+        # Feed all data query (select * from feed)
+        feed_query_set = Feed.objects.all()
+
+        # serialize feed_query_set (query_set -> dict)
+        feed_data = FeedSerializer(feed_query_set).data
+
+        return {'success': True, 'data': feed_data}
+
+    @staticmethod
+    def put(request):
+        body_data = request.data.get('key_name')
+        print(f'put body_data: {body_data}')
+
+        # feed data query (select * from feed where name=key_name)
+        feed_query_set = Feed.objects.filter(name='key_name')
+        if not feed_query_set.exists():
+            return {'success': False, 'message': 'not found data'}
+
+        # feed query 데이터에 첫번째 데이터를 가져옴 (filter return -> array)
+        feed_data = feed_query_set.first()
+        feed_data.name = body_data
+        feed_data.save()
+
+        return {'success': True}
+
+    @staticmethod
+    def post(request):
+        body_data = request.data.get('key_name')
+        print(f'post body_data: {body_data}')
+
+        # feed data create
+        Feed.objects.create(name=body_data)
+
+        return {'success': True}
+
+    @staticmethod
+    def delete(request):
+        body_data = request.data.get('key_name')
+        print(f'delete body_data: {body_data}')
+
+        feed_query_set = Feed.objects.filter(name='key_name')
+        if not feed_query_set.exists():
+            return {'success': False, 'message': 'not found data'}
+
+        feed_data = feed_query_set.first()
+        feed_data.delete()
+
+        return {'success': True}
